227cb51fe26c0422ea452356d399d875f9338c05
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-20 20:58:19 +0700

    #3 add subject controller

---
+++ uit_learn_backend/Controllers/SubjectsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using uit_learn_backend.Dbs;
+
+namespace uit_learn_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SubjectsController : ControllerBase
+    {
+        public SubjectsController(IMongoDbService service)
+        {
+        }
+
+    }
+}
