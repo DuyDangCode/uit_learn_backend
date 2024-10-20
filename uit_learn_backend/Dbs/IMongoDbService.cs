78ddc92a614390f00ce635e805126f39cfae96d6
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-20 20:54:58 +0700

    #2 connet mongodb

---
+++ uit_learn_backend/Dbs/IMongoDbService.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Driver;
+
+namespace uit_learn_backend.Dbs
+{
+    public interface IMongoDbService
+    {
+
+        IMongoCollection<T> GetCollection<T>(string name);
+        IMongoDatabase Database();
+    }
+}
