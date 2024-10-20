bbe137375555c82c230bcf86b1e9b445a3a40275
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-20 20:58:03 +0700

    #3 add subject model

---
+++ uit_learn_backend/Models/Subjects.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace uit_learn_backend.Models
+{
+    public class Subjects
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("subject_name")]
+        public string? Name { get; set; }
+
+        [BsonElement("subject_description")]
+        public string? Description { get; set; }
+
+        [BsonElement("subject_is_published")]
+        public bool IsPublished { get; set; }
+
+        [BsonElement("subject_is_deleted")]
+        public bool IsDeleted { get; set; }
+
+        [BsonElement("subject_thumb")]
+        public string? Thumb { get; set; }
+    }
+}
