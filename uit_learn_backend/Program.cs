b8838dbff34c1765aff50719ec77d54d1d2f2eee
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-21 15:37:18 +0700

    #5 add photo and delete photo function

--- uit_learn_backend/Program.cs
+++ uit_learn_backend/Program.cs
@@ -5,7 +5,7 @@ using uit_learn_backend.Repos;
 using uit_learn_backend.Services;
 
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 {
     builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection("MongoDb"));
@@ -27,7 +27,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ uit_learn_backend/Repos/IPhotoRepo.cs
@@ -0,0 +1,10 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace uit_learn_backend.Repos
+{
+    public interface IPhotoRepo
+    {
+        Task<ImageUploadResult> Upload(IFormFile file);
+        Task<DeletionResult> Deletion(string fileId);
+    }
+}
