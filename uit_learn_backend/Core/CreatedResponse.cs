a9e96e40300ae78a0c692a8bf759ceea592567c5
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-22 15:41:09 +0700

    #7 add created response

---
+++ uit_learn_backend/Core/CreatedResponse.cs
@@ -0,0 +1,14 @@
+﻿using uit_learn_backend.Constant;
+
+namespace uit_learn_backend.Core
+{
+    public record CreatedResponse<T> : BaseCustomResponse<T>
+    {
+        public CreatedResponse(string name, T data) : base(StatusCode.Created, MessageStatusCode.Created(name), data)
+        {
+        }
+        public CreatedResponse(string message, int statusCode, T data) : base(statusCode, message, data)
+        {
+        }
+    }
+}
