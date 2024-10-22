7fd178e2f25582dc278ab22ae187100ff14e4684
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-22 15:41:28 +0700

    #7 rename ok response

---
+++ uit_learn_backend/Core/OkResponse.cs
@@ -0,0 +1,17 @@
+﻿using uit_learn_backend.Constant;
+
+namespace uit_learn_backend.Core
+{
+    public record OkResponse<T> : BaseCustomResponse<T>
+    {
+        public OkResponse(T data) : base(StatusCode.Ok, MessageStatusCode.Ok, data)
+        {
+        }
+        public OkResponse(string message, T data) : base(StatusCode.Ok, message, data)
+        {
+        }
+        public OkResponse(string message, int statusCode, T data) : base(statusCode, message, data)
+        {
+        }
+    }
+}
