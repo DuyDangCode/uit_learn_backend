8d236e00eddb7967dc6d838bf320e85351e7a122
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-22 15:22:53 +0700

    #8 add message status code and status code

---
+++ uit_learn_backend/Constant/MessageStatusCode.cs
@@ -0,0 +1,11 @@
+﻿namespace uit_learn_backend.Constant
+{
+    public class MessageStatusCode
+    {
+        public static string Ok => "Ok";
+        public static string Created(string name) => $"Create {name} succesful";
+        public static string UnAuth => "Fail authentication";
+        public static string InternalServerError => "Something went wrong";
+        public static string NotFound(string name) => $"Not found {name}";
+    }
+}
---
+++ uit_learn_backend/Constant/StatusCode.cs
@@ -0,0 +1,11 @@
+﻿namespace uit_learn_backend.Constant
+{
+    public static class StatusCode
+    {
+        public static int Ok = 2000;
+        public static int Created = 2001;
+        public static int UnAuth = 4001;
+        public static int NotFound = 4004;
+        public static int InternalServerError = 5000;
+    }
+}
