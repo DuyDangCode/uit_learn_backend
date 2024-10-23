0ed579f4a13d6d94830bc881eb94b4d8ff075c2b
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-23 16:01:54 +0700

    #7 refactor error detail

--- uit_learn_backend/core/ErrorDetail.cs
+++ uit_learn_backend/core/ErrorDetail.cs
@@ -7,6 +7,17 @@ namespace uit_learn_backend.Core
         public int StatusCode { get; set; }
         public string? Message { get; set; }
 
+        public ErrorDetail()
+        {
+
+        }
+
+        public ErrorDetail(int statusCode, string? message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
