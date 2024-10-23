6ab3fbb533c745b06ce2c51fa46b636274fed67e
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-23 15:59:11 +0700

    #9 add attribute for checking id format

---
+++ uit_learn_backend/Attributes/IdStringAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace uit_learn_backend.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+        AllowMultiple = false)]
+    public class IdStringAttribute : StringLengthAttribute
+    {
+        public IdStringAttribute() : base(50)
+        {
+            MinimumLength = 24;
+        }
+    }
+}
