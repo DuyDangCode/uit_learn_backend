bb39099175046ff6ed2b26d1d43c3e4ebafe64ba
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-23 16:05:02 +0700

    #10 add extension for convert list subject to list subject dto

---
+++ uit_learn_backend/Extensions/ListExtension.cs
@@ -0,0 +1,13 @@
+﻿using uit_learn_backend.Dtos;
+using uit_learn_backend.Models;
+
+namespace uit_learn_backend.Extensions
+{
+    public static class ListExtension
+    {
+        public static List<SubjectDto> ConvertToSubjectDtoList(this List<Subject> subjects)
+            => subjects.Select(subject => new SubjectDto(subject))
+            .ToList();
+
+    }
+}
