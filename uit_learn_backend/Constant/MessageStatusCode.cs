b08a7b946e5cc4e185246bc81d0959bf490c33b1
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-23 16:00:46 +0700

    #7 add message code for bad request

--- uit_learn_backend/Constant/MessageStatusCode.cs
+++ uit_learn_backend/Constant/MessageStatusCode.cs
@@ -4,6 +4,8 @@
     {
         public static string Ok => "Ok";
         public static string Created(string name) => $"Create {name} succesful";
+        public static string Get(string name) => $"Get {name} succesful";
+        public static string Update(string name) => $"Update {name} succesful";
         public static string UnAuth => "Fail authentication";
         public static string InternalServerError => "Something went wrong";
         public static string NotFound(string name) => $"Not found {name}";
