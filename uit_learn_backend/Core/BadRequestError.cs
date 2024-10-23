e1806bde19f8b910598df7381a61c2de5540db14
Thành Duy <21520774@gm.uit.edu.vn>
2024-10-23 16:01:30 +0700

    #7 add bad request error class

---
+++ uit_learn_backend/Core/BadRequestError.cs
@@ -0,0 +1,10 @@
+﻿namespace uit_learn_backend.Core
+{
+    public class BadRequestError : ErrorDetail
+    {
+        public BadRequestError(string message) : base(Constant.StatusCode.BadRequest, message)
+        {
+
+        }
+    }
+}
